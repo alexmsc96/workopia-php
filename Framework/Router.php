d7e56c2d4df80da603c9336f3dcad21e90caa058
Alexandru Muscina <alexandru.muscina@webamboos.com>
2024-02-12 20:20:26 +0200

    Refactor routing logic and add error handling

--- Framework/Router.php
+++ Framework/Router.php
@@ -88,19 +88,51 @@ class Router
    * @return void
    */
 
-  public function route($uri, $method)
+  public function route($uri)
   {
+    $request_method = $_SERVER['REQUEST_METHOD'];
+
     foreach ($this->routes as $route) {
-      if ($route['uri'] === $uri && $route['method'] === $method) {
-        // Extract controller and controller method
-        $controller = "App\\Controllers\\" . $route['controller'];
-        $controllerMethod = $route['controllerMethod'];
 
-        // Instantiate the controller and call the method
-        $controllerInstance = new $controller;
-        $controllerInstance->$controllerMethod();
+      //Split the current uri into segments
+      $uriSegments = explode('/', trim($uri, '/'));
 
-        return;
+      //Split the route uri into segments
+      $routeSegments = explode('/', trim($route['uri'], '/'));
+
+      $match = true;
+
+      // Check if hte number of segments matches
+      if (count($uriSegments) === count($routeSegments) && strtoupper($route['method'] === $request_method)) {
+        $params = [];
+
+        $match = true;
+
+        for ($i = 0; $i < count($uriSegments); $i++) {
+          // If the URIs do not match and there is no parameter in the current segment
+          if ($routeSegments[$i] !== $uriSegments[$i] && !preg_match('/\{(.+?)\}/', $routeSegments[$i])) {
+            $match = false;
+            break;
+          }
+          // Check for the param and add to $params array
+          if (preg_match('/\{(.+?)\}/', $routeSegments[$i], $matches)) {
+            $params[$matches[1]] = $uriSegments[$i];
+          }
+        }
+
+        if ($match) {
+          // Extract controller and controller method
+          $controller = "App\\Controllers\\" . $route['controller'];
+          $controllerMethod = $route['controllerMethod'];
+
+          // Instantiate the controller and call the method
+          $controllerInstance = new $controller;
+          $controllerInstance->$controllerMethod($params);
+          return;
+        }
+
+      } else {
+
       }
     }
     ErrorController::notFound();
